99aeb6cb8e24af50fa7e912b2c44bdd3b15c81c8
Chris Troutner <chris.troutner@gmail.com>
2020-08-12 15:13:09 -0700

    Trying to get wallet info from the props

--- src/components/txhistory.js
+++ src/components/txhistory.js
@@ -1,40 +1,43 @@
-import React from 'react'
-import { Row, Col, Button } from 'adminlte-2-react'
+import React from "react";
+import { Row, Col, Button } from "adminlte-2-react";
 
-const BchWallet = typeof window !== 'undefined' ? window.SlpWallet : null
+const BchWallet = typeof window !== "undefined" ? window.SlpWallet : null;
 
 // let _this
 
 class TXHistory extends React.Component {
-  render () {
+  render() {
     return (
       <Row>
-        <Col sm={12} className='text-center mt-2 mb-2'>
+        <Col sm={12} className="text-center mt-2 mb-2">
           <Button
-            text='Get TX History'
-            type='primary'
-            className='btn-lg'
+            text="Get TX History"
+            type="primary"
+            className="btn-lg"
             onClick={this.handleGetTxHistory}
           />
         </Col>
       </Row>
-    )
+    );
   }
 
-  async handleGetTxHistory () {
+  async handleGetTxHistory() {
     try {
-      console.log('Entering handleGetTxHistory()')
+      const walletInfo = this.props.walletInfo
+      console.log(`walletInfo: `, walletInfo)
+
+      console.log("Entering handleGetTxHistory()");
       // debugger;
 
-      const txs = await BchWallet.getTransactions()
-      console.log(`txs: ${JSON.stringify(txs, null, 2)}`)
+      const txs = await BchWallet.getTransactions();
+      console.log(`txs: ${JSON.stringify(txs, null, 2)}`);
     } catch (err) {
       console.error(
-        'Error in gatsby-plugin-bch-tx-history/handleGetTxHistory(): ',
+        `Error in gatsby-plugin-bch-tx-history/handleGetTxHistory(): `,
         err
-      )
+      );
     }
   }
 }
 
-export default TXHistory
+export default TXHistory;
