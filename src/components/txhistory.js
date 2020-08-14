c70019ac2634e61397313fe46decc0ee72f0e4ad
Daniel Gonzalez <danielhumgon@gmail.com>
2020-08-14 02:35:04 -0400

    feat(plugin): Finished plugin PoC

--- src/components/txhistory.js
+++ src/components/txhistory.js
@@ -2,16 +2,21 @@ import React from 'react'
 import { Row, Col, Button } from 'adminlte-2-react'
 
 // const BchWallet = typeof window !== 'undefined' ? window.SlpWallet : null
-
+let _this
 class TXHistory extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
-
-    this.walletInfo = props.walletInfo
-    console.log('constructor walletInfo: ', props.walletInfo)
+    _this = this
+    this.state = {
+      transactions: [],
+      bchWallet: props.bchWallet
+    }
+    // this.walletInfo = props.walletInfo
+    // console.log('constructor walletInfo: ', props.walletInfo)
   }
 
-  render () {
+  render() {
+    const { transactions } = _this.state
     return (
       <Row>
         <Col sm={12} className='text-center mt-2 mb-2'>
@@ -22,15 +27,59 @@ class TXHistory extends React.Component {
             onClick={this.handleGetTxHistory}
           />
         </Col>
+
+        {/* Show the transactions */}
+        <Col sm={12}>
+          {
+            transactions.map(val => {
+              return <div key={val} className='text-center mt-1 mb-1'>
+                {val}
+              </div>
+            })
+          }
+        </Col>
+
+        { /*Close the transaction list*/}
+        <Col className='text-center mt-2 mb-2'>
+        {
+            transactions.length > 0 && (
+              <Button
+                text='Close'
+                type='primary'
+                className='btn-lg'
+                onClick={this.handleCleanTransactions}
+              />
+            )
+          }
+        </Col>
+
       </Row>
     )
+  }
+  componentDidUpdate() {
+    //Update state if a new wallet is detected
+    if (_this.state.bchWallet !== _this.props.bchWallet) {
+      _this.setState({
+        bchWallet: _this.props.bchWallet, // Update wallet instance
+        transactions: [] // Clean transaction array if a new wallet is detected
+      })
+    }
   }
 
   // This event handler would use the minimal-slp-wallet-web library to retrieve
   // the transaction history for the address.
-  async handleGetTxHistory () {
+  async handleGetTxHistory() {
     try {
       console.log('Entering handleGetTxHistory()')
+
+      const { bchWallet } = _this.state
+      if (!bchWallet) throw new Error('Wallet not found!')
+
+      const transactions = await bchWallet.getTransactions();
+
+      _this.setState({
+        transactions
+      })
     } catch (err) {
       console.error(
         'Error in gatsby-plugin-bch-tx-history/handleGetTxHistory(): ',
@@ -38,6 +87,13 @@ class TXHistory extends React.Component {
       )
     }
   }
+  // Clean the transaction list with an empty array
+  handleCleanTransactions() {
+    _this.setState({
+      transactions: []
+    })
+  }
 }
 
 export default TXHistory
+
